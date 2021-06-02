c69df7500cf3249d6ef53fa0a12be1b83e3a9f75
Vinit Patel <vinit@Vinits-MacBook-Pro.local>
2021-06-02 19:59:19 -0400

    changes to bar graph

--- playground/client/BarDisplay.tsx
+++ playground/client/BarDisplay.tsx
@@ -1,54 +1,76 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import "chartjs-plugin-streaming";
+import React, { useState, useEffect } from 'react'
+import {Bar} from 'react-chartjs-2'
+let flag = true
+const DynamicChart = () => {
+    const [chartData, setChartData]  = useState({});
 
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+    
 
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-    ],
-  },
-};
+ const Chart = () => {
+   console.log("chart called")
+    let values = []
+      if(flag){
+        values = [20, 10, 15];
+        flag = false
+      }
+      else {
+         values = [30, 10, 15];
+        flag = true
+      }
+        console.log(values)
+        console.log(flag)
+     
+            setChartData({
+                labels: ["01", "02", "03"],
+                datasets: [{
+                                             label: 'level of thicceness',
+                                             data: values,
+                                             backgroundColor: [
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)'
+                      
+                                                
+                                             ],
+                                             borderColor: [
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)'
+                                               
+                                             ],
+                                             borderWidth: 1
+                                         }]
+            });
+        
+   
+        
+    }
+    useEffect(() => {
+        Chart();
+      }, []);
+      return(
+          <div className="App">
+              <h1>Bar Chart</h1>
+              <div>
+                <button onClick = {()=>{Chart()}}> click</button>
+                  <Bar
+                  
+                    data={chartData}
+                    options={{
+                        responsive:true,
+                        title: { text: "THICCNESS SCALE", display: true },
+                        scales:{
+                            yAxes:{
+                                ticks:{
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    }}
+                  />
+              </div>
+          </div>
+      )
+}
 
-const VerticalBar = () => (
-  <>
-    <div className='header'>
-      <h1 className='title'>Vertical Bar Chart</h1>
-
-    </div>
-    <Bar data={data} options={options} />
-  </>
-);
-
-export default VerticalBar;
+export default DynamicChart;
